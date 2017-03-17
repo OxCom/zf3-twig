53037bd7922cb37f5543200d6a6509e1dbe42bcf
OxCom <gm.oxcom@gmail.com>
2017-03-17 23:44:58 +0100

    fix cs

--- tests/Fixture/config/autoload/global.php
+++ tests/Fixture/config/autoload/global.php
@@ -3,9 +3,9 @@
 return [
     'view_manager' => [
         'doctype'                 => \Zend\View\Helper\Doctype::HTML5,
-        'template_map' => array (
+        'template_map'            => [
             'layout' => __DIR__ . '/../../view/Map/layout.twig',
-        ),
+        ],
         'template_path_stack'     => [
             'ZendTwig' => __DIR__ . '/../../view/ZendTwig',
         ],
