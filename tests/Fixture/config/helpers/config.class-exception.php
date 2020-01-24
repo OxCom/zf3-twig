4307405fcca2222478e139a538ac4888629f3ceb
oxcom <andrii@crytek.de>
2020-01-24 12:10:36 +0100

    fix: cs

--- tests/Fixture/config/helpers/config.class-exception.php
+++ tests/Fixture/config/helpers/config.class-exception.php
@@ -1,29 +1,35 @@
 <?php
 
+use Laminas\Navigation\View\HelperConfig;
+use Laminas\ServiceManager\Factory\InvokableFactory;
+use ZendTwig\Service\TwigLoaderFactory;
+use ZendTwig\Test\Fixture\DummyClass;
+use ZendTwig\Test\Fixture\DummyClassInvokable;
+
 return [
     'service_manager' => [
         'factories' => [
-            \ZendTwig\Test\Fixture\DummyClassInvokable::class => \Laminas\ServiceManager\Factory\InvokableFactory::class,
+            DummyClassInvokable::class => InvokableFactory::class,
         ],
     ],
     'view_manager'    => [
         'template_path_stack'     => [
             'ZendTwig' => __DIR__ . '/../../view/ZendTwig',
         ],
-        'default_template_suffix' => \ZendTwig\Service\TwigLoaderFactory::DEFAULT_SUFFIX,
+        'default_template_suffix' => TwigLoaderFactory::DEFAULT_SUFFIX,
     ],
     'zend_twig'       => [
         'helpers' => [
             'configs' => [
-                \Laminas\Navigation\View\HelperConfig::class,
-                \ZendTwig\Test\Fixture\DummyClassInvokable::class,
+                HelperConfig::class,
+                DummyClassInvokable::class,
                 [
                     'aliases'    => [],
                     'delegators' => [],
                     'factories'  => [],
                 ],
                 // will \ZendTwig\Service\TwigHelperPluginManagerFactory
-                \ZendTwig\Test\Fixture\DummyClass::class,
+                DummyClass::class,
             ],
         ],
     ],
--- tests/Fixture/config/helpers/config.instance-exception.php
+++ tests/Fixture/config/helpers/config.instance-exception.php
@@ -1,22 +1,27 @@
 <?php
 
+use Laminas\Navigation\View\HelperConfig;
+use Laminas\ServiceManager\Factory\InvokableFactory;
+use ZendTwig\Service\TwigLoaderFactory;
+use ZendTwig\Test\Fixture\DummyClassInvokable;
+
 return [
     'service_manager' => [
         'factories' => [
-            \ZendTwig\Test\Fixture\DummyClassInvokable::class => \Laminas\ServiceManager\Factory\InvokableFactory::class,
+            DummyClassInvokable::class => InvokableFactory::class,
         ],
     ],
     'view_manager'    => [
         'template_path_stack'     => [
             'ZendTwig' => __DIR__ . '/../../view/ZendTwig',
         ],
-        'default_template_suffix' => \ZendTwig\Service\TwigLoaderFactory::DEFAULT_SUFFIX,
+        'default_template_suffix' => TwigLoaderFactory::DEFAULT_SUFFIX,
     ],
     'zend_twig'       => [
         'helpers' => [
             'configs' => [
-                \Laminas\Navigation\View\HelperConfig::class,
-                \ZendTwig\Test\Fixture\DummyClassInvokable::class,
+                HelperConfig::class,
+                DummyClassInvokable::class,
                 // will \ZendTwig\Service\TwigHelperPluginManagerFactory
                 42,
             ],
